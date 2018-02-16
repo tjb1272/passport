ef38b3d81098ca5655931836e29e240544382492
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-02-16 16:20:47 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/register', function(req, res) {
-    res.render('register');
+router.get('/account', function(req, res) {
+    res.render('account');
 });
 
 router.get('/login', function(req, res) {
     res.render('login');
 });
 
-router.post('/register', function(req, res) {
-    res.render('login');
-    const name = req.body.name;
-    const email = req.body.email;
+router.post('/account', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const password2 = req.body.password2;
 
-    console.log(name);
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equales(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('account', {
+            errors:errors
+        });
+    } else {
+        var newUser = new userInfo({
+            username: username,
+            password: password
+        });
+        User.createUser(newUser, function(err, user) {
+            if (err) throw err;
+            console.log (user);
+        });
+        req.flash('success_msg', 'You have successfully registered')
+        res.redirect('/users/login');
+    }
 });
 
 module.exports = router;
